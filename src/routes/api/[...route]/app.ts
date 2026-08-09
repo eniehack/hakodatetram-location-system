4fc040210abaff933e7b87eee3e730e3e14b34af
Kuuya Niinobe <niinobe@mierune.co.jp>
2026-08-09 20:14:43 +0900

    chore: d1依存をやめたことによる不要なパッケージ、エンドポイントの削除

--- src/routes/api/[...route]/app.ts
+++ src/routes/api/[...route]/app.ts
@@ -2,9 +2,6 @@ import { Hono } from 'hono';
 import { streamSSE } from 'hono/streaming';
 import gtfsRealtimeBindings from 'gtfs-realtime-bindings';
 import { EventEmitter } from 'events';
-import type { D1Database } from '@cloudflare/workers-types';
-import * as v from 'valibot';
-import { vValidator } from '@hono/valibot-validator';
 
 const emitter = new EventEmitter();
 
@@ -48,28 +45,7 @@ setInterval(async () => {
 	emitter.emit('trip-update', data);
 }, 1000 * 30);
 
-type Bindings = {
-	DB: D1Database;
-};
-
-const tripParamsSchema = v.object({
-	id: v.string()
-});
-
-export const router = new Hono<{ Bindings: Bindings }>()
-	.get('/trip', vValidator('query', tripParamsSchema), async (c) => {
-		console.debug('trip');
-		const { id: encodedId } = c.req.valid('query');
-		console.debug(encodedId);
-		const id = decodeURIComponent(encodedId);
-		const result = (await c.env.DB.prepare('SELECT trip_headsign FROM trips WHERE trip_id = ?')
-			.bind(id)
-			.first()) as null | { trip_headsign: string };
-		if (!result) {
-			return c.json({}, 404);
-		}
-		return c.json({ headsign: result.trip_headsign }, 200);
-	})
+export const router = new Hono()
 	.get('/rt', async (c) => {
 		return streamSSE(c, async (stream) => {
 			const vehicleUpdate = (data) => {
@@ -86,13 +62,12 @@ export const router = new Hono<{ Bindings: Bindings }>()
 			};
 			emitter.on('vehicle-update', vehicleUpdate);
 			emitter.on('trip-update', tripUpdate);
-			while (true) {
-				await new Promise((resolve) => setTimeout(resolve, 1000 * 30));
-			}
-
 			stream.onAbort(() => {
 				emitter.off('data-update', vehicleUpdate);
 			});
+			while (true) {
+				await new Promise((resolve) => setTimeout(resolve, 1000 * 30));
+			}
 		});
 	});
 
